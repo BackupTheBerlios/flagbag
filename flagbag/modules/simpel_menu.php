a3f7bfdcc11a880769426627a4e4b060707b8100
blambi <blambi>
2005-06-09 15:01:34 +0000

    fixed some errors

--- flagbag/modules/simpel_menu.php
+++ flagbag/modules/simpel_menu.php
@@ -1,14 +1,13 @@
 <?php
   /*
    simple_menu.php
-   Version 0.5.1
    Copyright (C) 2005
    Niklas Lindblad <niklas.lindblad@gmail.com>
    Patrik Nilsson  <putte@home.frozentux.net>
    Released under the GNU General Public License version 2
   */
   
-  /* DECLARE ELEMENTS TO USE IN THE MENU */
+  /* DECLARE ELEMENTS THATS SHOULD BE USED */
 	/* echo "<p><i>Menu</i></p>\n"; */
 	/* echo "<hr />"; */
 ?>
