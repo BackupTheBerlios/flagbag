b65a609416dc5e6a250306208e685fee2587965b
blambi <blambi>
2005-06-14 11:27:36 +0000

    added some comments and did some small code adjustments

--- flagbag/modules/singel_element.php
+++ flagbag/modules/singel_element.php
@@ -1,8 +1,12 @@
 <?php
-	$msg = "endast denna";
-	$msg_main = "tillbaka";
+	/* This module adds a link for every element thats transports you to
+	   and form a singel blogg element. */
+	
+	$msg_only = "endast denna"; // Only this one
+	$msg_main = "tillbaka"; // Return
+	
 	if ($max!=$min) {
-		echo "<a href=\"$url/?max=$numb&amp;min=$numb\">$msg</a>";
+		echo "<a href=\"$url/?max=$numb&amp;min=$numb\">$msg_only</a>";
 	}
 	else {
 		echo "<a href=\"$url/\">$msg_main</a>";
