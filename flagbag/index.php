f461633ba7b81c85e044e0890e8045af2f9d1610
falcon <falcon>
2005-06-15 23:27:38 +0000

    Cleaned up after myself

--- flagbag/index.php
+++ flagbag/index.php
@@ -87,7 +87,6 @@ echo $now; ?>
 		   $lines5 = file("contain/blog$numb");
 		    foreach ($lines5 as $line_num5 => $line5) {
 			    echo $line5;
-			    /*echo "<br/>";*/
 		    }
                 if ( file_exists('modules/element_mod.php') )
          		include 'modules/element_mod.php';
