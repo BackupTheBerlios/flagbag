b1eb5919421dab634908116a781003a7f7fbfcf1
nlindblad <nlindblad>
2005-06-26 19:39:46 +0000

    EASIER TO READ

--- flagbag/index.php
+++ flagbag/index.php
@@ -1,76 +1,123 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="sv"> 
-<!-- 
-  index.php 
-  Copyright (C) Niklas Lindblad and Patrik Nilsson 2005
-  Released under the GNU General Public License version 2
--->
-<?php
+<HTML XMLNS="http://www.w3.org/1999/xhtml" XML:LANG="sv"> 
+<?PHP
+  /* THIS WON'T BE DISPLAYED ALONG WITH THE XHTML-CODE, ALL 
+     TEXT INSIDE THE '<?php'-TAGS ARE FOR YOUR EYES ONLY.
+     
+     THE FOLLOWING CODE IS LICENSED UNDER THE GNU GENERAL PUBLIC LICENSE VERSION 2.
+     YOU MAY MODIFY AND DISTRIBUTE IT UNDER THE TERMS OF THE LICENSE. FULL LICENSE
+     CAN BE FOUND HERE: http://www.gnu.org/licenses/gpl.txt
+     
+     THIS FILE IS A PART OF THE FLAGBAG BLOG-ENGINE PROJECT: http://developer.berlios.de/projects/flagbag
+     
+     COPYRIGHT (C) 2005 THE DEVELOPERS OF FLAGBAG
+  */
+     
+  /* BEGIN INCLUDES */
 
-  /* MAKE INCLUDES */
 
-  /* zer.php holds all our variables and data */
-  /* If it exists we'll include it */
+  /* IF FOUND WE'LL INCLUDE ./php/zer.php */
       if ( file_exists('php/zer.php') ) 
          include 'php/zer.php';
 
-  /* We also include the silent modules */
+  /* BEGIN INCLUDING SILENT MODULES */
       if ( file_exists('modules/silent_mod.php') )
 	 include 'modules/silent_mod.php';
+  /* END INCLUDING SILENT MODULES */
+  
+  /* ENABLE THE MIN/MAX-FUNCTION FOR ELEMENTS */
 
-  /* ENABLE THE MIN/MAX-FUNCTION */
-
-  /* Read some variables who could be modified by for example plugins */
-	$get_max=$_GET['max'];
+  /* READ SOME VARIABLES WHO COULD BE MODIFIED BY PLUGINS */
+  	$get_max=$_GET['max'];
 	$get_min=$_GET['min'];
-
-  /* The real HTML-code starts below, remember that <html> is already opened */
+  /* END READING VARIABLES */
+  
+  
+  /* END INCLUDES
 ?>
-    
-<head>
-  <title><?php /* Print title */ echo $title; ?></title>
-<link rel="stylesheet" type="text/css" href="<?php /* Set CSS */ echo $style_sheet; ?>" title="layout" />
-      <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-</head>
+
+
+<!-- =============== -->
+<!-- HEAD TAG BEGINS -->
+<!-- =============== -->
+
+<HEAD>
+  <TITLE><?php echo $title; ?></TITLE>
+<LINK REL="stylesheet" TYPE="text/css" HREF="<?php echo $style_sheet; ?>" TITLE="layout" />
+      <!-- BEGIN OF META TAG -->
+      <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=iso-8859-1" />
+      <!-- END OF META TAG -->
+</HEAD>
 	
-<body>
-<div id="container">
-<!-- Title and menu -->
+<!-- ============= -->
+<!-- HEAD TAG ENDS -->
+<!-- ============= -->
+	
 
-<div id="title">
-<?php /* Echo title */
-echo $title; ?>
-</div>
-<div id="date">
-<?php /* Echo date */
-echo $now; ?>
-</div>
-</div>
-<!-- Menu_mod -->
-<?php
-/* If the menu_mod exists load it (displays a menu at the top of the page */
+<!-- =========== -->
+<!-- BODY BEGINS -->
+<!-- =========== -->
+
+<BODY>
+
+<!-- ================ -->
+<!-- CONTAINER BEGINS -->
+<!-- ================ -->
+
+<DIV ID="container">
+
+<!-- PRINTING PAGE TITLE -->
+<DIV ID="title">
+<?php echo $title; ?>
+</DIV>
+<!-- DONE PRINTING PAGE TITLE -->
+
+<!-- PRINTING CURRENT DATE -->
+<DIV ID="date">
+<?php echo $now; ?>
+</DIV>
+<!-- DONE PRINTING CURRENT DATE -->
+
+</DIV>
+
+<!-- ============== -->
+<!-- CONTAINER ENDS -->
+<!-- ============== -->
+
+<?PHP
+  /* MENU PRINTING BEGINS */
+  /* THE FOLLOWING CODE WILL LOAD THE MODULE THAT HANDLES THE MENUS, AGAIN, ONLY IF IT EXISTS */
+
+  /* IF THE FILE ./modules/menu_mod.php EXISTS THEN INClUDE IT AND PRINT A FEW COMMENTS IN THE XHTML-CODE */ 
 if ( file_exists('modules/menu_mod.php') ) {
-	echo "<div id=\"menu\">";
+        echo "<!-- ============ -->\n";
+        echo "<!-- MENUS BEGINS -->\n";
+        echo "<!-- ============ -->\n";
+        echo "\n";
+	echo "<DIV ID=\"menu\">\n";
 	include 'modules/menu_mod.php';
-	echo "</div>";
+	echo "</DIV>";
+	echo "\n";
+	echo "<!-- ========== -->\n";
+        echo "<!-- MENUS ENDS -->\n";
+        echo "<!-- ========== -->\n";
  }
-?>
-<!-- End -->
-<!-- Text -->
-<?php
- 
-  /* BLOG-ARTICLES PRINTING */
 
-  /* Articles are stored in files named blog<nr> they need to include their own formatting. */
+   /* END OF MENU PRINTING */ 
+    
+  
+  /* BLOG-ARTICLES PRINTING BEGINS */
 
-  /* If 'containt/last.blog' exists then make the last blog-article to last.blog instead */
+  /* EACH ARTIClE IS LOADED FROM A FILE NAMED LIKE: blog$NUMBER */
+  
+  /* IF ./contain/last.blog EXISTS THEN MAKE THE LAST BLOG-ARTICLE TO last.blog INSTEAD */
    if (file_exists("contain/last.blog") ) {
 	   $lastnr = file("contain/last.blog");
 	   $max = ereg_replace("\n", "", $lastnr[0]);
 	   if ($max>16) {
 		   $min=$max-16;
 	   }
-  /* Else, don't and set $min to 0 */
+  /* ELSE DON'T SET $min TO 0 */
 	   else {
 		   $min=0;
 	   }
@@ -81,32 +128,55 @@ if ( file_exists('modules/menu_mod.php') ) {
    }
 
    if (($get_max) && ($get_min)) {
-	/* If the get_max and $get_min exists then these values would
-	   Over run the "default" $max and $min, but only if $get_max IS larger
-	   or the same size as $get_min. */
+	/* 
+	   IF THE $get_max AND $get_min EXISTS THEN THESE VALUES WOULD OVERRUN THE DEFAULT $max AND $min
+	   BUT ONLY IF $get_max IS LARGER OR THE SAME SIZE AS $get_min. 
+	*/
 	   if ($get_max >= $get_min) {
 		   $max = $get_max;
 		   $min = $get_min;
 	   }
    }
-
+   
    for ( $numb = $max ; $numb >= $min ; $numb-- ) {
 	   if (file_exists("contain/blog$numb") ) {
-		   echo "<div class=\"post-container\">";
+	                   echo "<!-- =============== -->\n";
+                           echo "<!-- ARTICLES BEGINS -->\n";
+                           echo "<!-- =============== -->\n";
+                           echo "\n";
+		   echo "<DIV CLASS=\"post-container\">";
 		   $lines5 = file("contain/blog$numb");
 		    foreach ($lines5 as $line_num5 => $line5) {
 			    echo $line5;
 		    }
                     if ( file_exists('modules/element_mod.php') )
                     	   include 'modules/element_mod.php';
-		    echo "</div>";
-	   }
+		    echo "</DIV>";
+		    echo "\n";
+		    echo "<!-- ============= -->\n";
+                    echo "<!-- ARTICLES ENDS -->\n";
+                    echo "<!-- ============= -->\n";
+                    echo "\n";
+                    }
    }	       
 ?>
-<!-- Footer -->
-<div id="footer">
-<?php echo $tag ?> using <?php echo $php_version ?> and Valid <a href="http://validator.w3.org/check?uri=referer">XHTML 1.1</a> and <a href="http://jigsaw.w3.org/css-validator/validator?uri=<?php echo $url ?>">CSS</a>
-</div>
-<!-- End -->
-</body>
-</html>
+<!-- ================== -->
+<!-- SITE FOOTER BEGINS -->
+<!-- ================== -->
+
+<DIV ID="footer">
+<?php echo $tag ?> using <?php echo $php_version ?> and Valid<a href="http://validator.w3.org/check?uri=referer"> XHTML 1.1</a> and<a href="http://jigsaw.w3.org/css-validator/validator?uri=<?php echo $url ?>"> CSS</a>
+</DIV>
+
+<!-- ================ -->
+<!-- SITE FOOTER ENDS -->
+<!-- ================ -->
+
+
+</BODY>
+
+<!-- ========= -->
+<!-- BODY ENDS -->
+<!-- ========= -->
+
+</HTML>
