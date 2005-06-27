1dc94ed2b897bb1d66813110e2b4df1998b51179
nlindblad <nlindblad>
2005-06-27 12:56:12 +0000

    Added more comments, code is now more efficient to read and in the long run this will save us some time.

--- flagbag/log.php
+++ flagbag/log.php
@@ -1,35 +1,86 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="sv"> 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="sv">
 <?php
-/* Include the logger itself */
-/* This page only displays the output from php/logger.php */
-/* Include the php/logger.php in sites that needs logging */
-include 'php/zer.php';
-/* Include to get the right style-sheet :) */
-?>
-<head>
-<title> Log for <?php echo $title ?></title>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<link rel="stylesheet" type="text/css" href="<?php /* Load CSS */ echo $style_sheet ?>" title="layout" />
-</head>
-<body>
-<div id="container">
-<!-- Title and menu -->
+  /* THIS WON'T BE DISPLAYED ALONG WITH THE XHTML-CODE, ALL */
+  /* TEXT INSIDE THE '<?php'-TAGS ARE FOR YOUR EYES ONLY. */
+     
+  /* THE FOLLOWING CODE IS LICENSED UNDER THE GNU GENERAL PUBLIC LICENSE VERSION 2. */
+  /* YOU MAY MODIFY AND DISTRIBUTE IT UNDER THE TERMS OF THE LICENSE. FULL LICENSE */
+  /* CAN BE FOUND HERE: http://www.gnu.org/licenses/gpl.txt */
+     
+  /* THIS FILE IS A PART OF THE FLAGBAG BLOG-ENGINE PROJECT: http://developer.berlios.de/projects/flagbag */
+     
+  /* COPYRIGHT (C) 2005 THE DEVELOPERS OF FLAGBAG */
+     
+  /* BEGIN INCLUDES */
 
+
+  /* IF FOUND WE'LL INCLUDE ./php/zer.php */
+      if ( file_exists('php/zer.php') ) 
+         include 'php/zer.php';
+
+
+  /* END INCLUDES */
+?>
+
+<!-- =============== -->
+<!-- HEAD TAG BEGINS -->
+<!-- =============== -->
+
+<head> 
+<title>Log for <?php /* Print title */ echo $title; ?></title>
+<link rel="stylesheet" type="text/css" href="<?php /* Set CSS */ echo $style_sheet; ?>" title="layout" />
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+</head>
+	
+<!-- ============= -->
+<!-- HEAD TAG ENDS -->
+<!-- ============= -->
+	
+
+<!-- =========== -->
+<!-- BODY BEGINS -->
+<!-- =========== -->
+
+<body>
+
+<!-- ================ -->
+<!-- CONTAINER BEGINS -->
+<!-- ================ -->
+
+<div id="container">
+
+<!-- PRINTING PAGE TITLE -->
 <div id="title">
-<?php /* Echo title */
-echo 'Log for '; echo $title; ?>
+Log for <?php echo $title; ?>
 </div>
+<!-- DONE PRINTING PAGE TITLE -->
+
+<!-- PRINTING CURRENT DATE -->
 <div id="date">
-<?php /* Echo date */
-echo $now; ?>
+<?php echo $now; ?>
 </div>
+<!-- DONE PRINTING CURRENT DATE -->
+
 </div>
+
+<!-- ============== -->
+<!-- CONTAINER ENDS -->
+<!-- ============== -->
+
+
+<!-- ===================== -->
+<!-- POST-CONTAINER BEGINS -->
+<!-- ===================== -->
 <div class="post-container">
+
+<!-- LOG DATA OUTPUT BEGINS -->
 <table>
 <?php
-/* Log-file created by php/logger.php
-/* If the log-file exists then format it line-by-line and then print it here*/
+
+  /* THE FOLLOWING FUNCTION PARSES AND PRINT THE LOGFILE LINE BY LINE */
+  /* FIRST OFF IT CHECKS IF THE LOG-FILE EXISTS. */
+
   if (file_exists('logs/logger_silent.log') ) {
 	  $logdata = file('logs/logger_silent.log');
 	  $lines=count($logdata);
@@ -37,14 +88,33 @@ echo $now; ?>
 		  echo $logdata[$numb];
 	  }
   }
-  ?>
+
+?>
 </table>
 </div>
-<hr />
+<!-- END OF LOG DATA OUTPUT -->
+<!-- =================== -->
+<!-- POST-CONTAINER ENDS -->
+<!-- =================== -->
+
+
+<!-- ================== -->
+<!-- SITE FOOTER BEGINS -->
+<!-- ================== -->
+
 <div id="footer">
-<?php echo $tag ?> using <?php echo $php_version ?>
-<i>Valid <a href="http://validator.w3.org/check?uri=referer">XHTML 1.1</a>and <a href="http://jigsaw.w3.org/css-validator/validator?uri=http://linux.rwarforums.com/blog/utveckling/css/layout.css">CSS</a></i>
-<?php echo $client ?>
+<?php echo $tag ?> using <?php echo $php_version ?> and Valid <a href="http://validator.w3.org/check?uri=referer">XHTML 1.1</a> and <a href="http://jigsaw.w3.org/css-validator/validator?uri=<?php echo $url ?>">CSS</a>
 </div>
+
+<!-- ================ -->
+<!-- SITE FOOTER ENDS -->
+<!-- ================ -->
+
+
 </body>
+
+<!-- ========= -->
+<!-- BODY ENDS -->
+<!-- ========= -->
+
 </html>
